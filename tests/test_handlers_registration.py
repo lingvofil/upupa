26b022b9060e2b557a4b87d798087a441f2f18ef
lingvofil <134329366+lingvofil@users.noreply.github.com>
2026-09-10 15:10:32 +0300

    Refresh social graph handler test comment

--- tests/test_handlers_registration.py
+++ tests/test_handlers_registration.py
@@ -1,7 +1,7 @@
 """Регрессия этапа 3: состав и порядок регистрации хэндлеров контролируются явно."""
 from tests import test_smoke_imports  # noqa: F401  (env + моки)
 
-EXPECTED_TOTAL_HANDLERS = 151  # + всратый соцграф
+EXPECTED_TOTAL_HANDLERS = 151  # + соцграф рисунок
 
 
 def _count_handlers(router):
