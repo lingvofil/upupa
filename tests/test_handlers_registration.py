01e3dad5d735ef4b421766d93a8ef5c2e26c3cff
lingvofil <134329366+lingvofil@users.noreply.github.com>
2026-09-10 14:58:19 +0300

    Обновить ожидаемое число хэндлеров

--- tests/test_handlers_registration.py
+++ tests/test_handlers_registration.py
@@ -1,7 +1,7 @@
 """Регрессия этапа 3: состав и порядок регистрации хэндлеров контролируются явно."""
 from tests import test_smoke_imports  # noqa: F401  (env + моки)
 
-EXPECTED_TOTAL_HANDLERS = 150  # + priority Crocodile answer handlers
+EXPECTED_TOTAL_HANDLERS = 151  # + всратый соцграф
 
 
 def _count_handlers(router):
